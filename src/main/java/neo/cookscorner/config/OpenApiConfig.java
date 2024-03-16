597dc43aaec023c39b5780dc9dc701dc826ae880
Nurik <nalymbaide@gmail.com>
2024-03-16 11:27:55 +0600

    impoved swagger config

--- src/main/java/neo/cookscorner/config/OpenApiConfig.java
+++ src/main/java/neo/cookscorner/config/OpenApiConfig.java
@@ -4,7 +4,15 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.Collections;
 
+@Configuration
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact(
@@ -23,5 +31,24 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 )
         }
 )
+
 public class OpenApiConfig {
+    private static final String API_KEY = "Bearer Token";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(API_KEY, apiKeySecuritySchema()))
+                .security(Collections.singletonList(new SecurityRequirement().addList(API_KEY)));
+    }
+
+    private SecurityScheme apiKeySecuritySchema() {
+        return new SecurityScheme()
+                .name("Authorization")
+                .description("JWT token for authentication")
+                .in(SecurityScheme.In.HEADER)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer");
+    }
 }
